60a5c82efaa2e3a98cb3817614e688f8e9642a33
skawlgns <skawlgns@kakao.com>
2022-01-10 18:34:29 +0900

    ~single-dimensional array

--- array/3052.js
+++ array/3052.js
@@ -1,3 +1,7 @@
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-const input = fs.readFileSync(filePath).toString().split("\n");
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+const numbers = [];
+input.forEach((n) => numbers.indexOf(n % 42) === -1 && numbers.push(n % 42));
+console.log(numbers.length);
---
+++ array/8958.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = fs.readFileSync(filePath).toString().split("\n");
+
+const num = Number(input[0]);
+
+for (let i = 1; i <= num; i++) {
+  let count = 0;
+  let sum = 0;
+
+  for (let j = 0; j < input[i].length; j++) {
+    if (input[i][j] === "O") {
+      count++;
+    } else {
+      count = 0;
+    }
+    sum += count;
+  }
+  console.log(sum);
+}
